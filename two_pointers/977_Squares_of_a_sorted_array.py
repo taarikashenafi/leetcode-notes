40e0565e23f99bf75ab68436654b701a8dd690a4
Taarik Ashenafi <147209483+taarikashenafi@users.noreply.github.com>
2024-10-13 22:45:45 -0500

    update solution

--- two_pointers/977_Squares_of_a_sorted_array.py
+++ two_pointers/977_Squares_of_a_sorted_array.py
@@ -13,9 +13,9 @@ class Solution(object):
         if n == 0: return []
         # initialize two pointers, i and j, to the start and end of the array
         i,j = 0,n-1
-        # create a result array to store our answer
+        # create a result array the same size as the input array to store our answer
         res = [0] * n
-        # loop through the array in reverse order
+        # loop through the array in reverse order, starting from the end
         for k in reversed(range(n)):
             # calculate the squares of the elements at i and j
             i_sqrd = nums[i]**2
