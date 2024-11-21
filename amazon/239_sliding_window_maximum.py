6abdb7dc1d407b10c25dffa0ea0e65d3300a18ef
Taarik Ashenafi <147209483+taarikashenafi@users.noreply.github.com>
2024-11-20 23:58:57 -0600

    239. Sliding Window Maximum

---
+++ amazon/239_sliding_window_maximum.py
@@ -0,0 +1,23 @@
+# 239. Sliding Window Maximum
+# https://leetcode.com/problems/sliding-window-maximum
+
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        d = collections.deque()
+        out = []
+        for i, n in enumerate(nums):
+            while d and nums[d[-1]] < n:
+                d.pop()
+            d += i,
+            if d[0] == i - k:
+                d.popleft()
+            if i >= k - 1:
+                out += nums[d[0]],
+        return out
